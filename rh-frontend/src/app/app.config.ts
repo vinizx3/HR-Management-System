051d1e4a982ce89d11748dfa79d0634d954653db
vinicius <fernadesvinicius7298@gmail.com>
2026-06-10 02:04:02 -0300

    feat: add environment config and api interceptor for production

--- rh-frontend/src/app/app.config.ts
+++ rh-frontend/src/app/app.config.ts
@@ -3,15 +3,16 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/services/auth.interceptor';
+import { apiInterceptor } from './core/interceptors/api.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([apiInterceptor, authInterceptor])),
     {
-    provide: LOCALE_ID,
-    useValue: 'pt-BR'
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
     }
   ]
 };
---
+++ rh-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://hr-management-system-4smu.onrender.com'
+};
